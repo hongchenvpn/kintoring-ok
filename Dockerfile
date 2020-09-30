7bc5db2199a3bb366865f7443064a6d7a0dfd036
ringring1 <71864538+ringring1@users.noreply.github.com>
2020-09-30 09:10:11 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,23 @@
 FROM alpine:edge
 
-ENV V2RAYPATH   /v2raypath
+ENV PORT        3000
+ENV GOSTPATH    /gostpath
+ENV PASSWORD    password
+
 
 RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-    apk update && apk add --no-cache ca-certificates caddy && \
-    rm -rf /var/cache/apk/* && \
-    mkdir -p /wwwroot
-ADD entry.sh /entry.sh
-ADD index.html /wwwroot/index.html
-RUN chmod +x /entry.sh
+    apk update && apk add --no-cache ca-certificates caddy shadowsocks-libev && \
+    v2rayplugin_URL="$(wget -qO- https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest | grep -E "browser_download_url.*linux-amd64" | cut -f4 -d\")" && \
+    wget -O - $v2rayplugin_URL | tar -xz -C /usr/bin/ && \
+    gost_URL="$(wget -qO- https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -E "browser_download_url.*linux-amd64" | cut -f4 -d\")" && \
+    wget -O - $gost_URL | gzip -d > /usr/bin/gost && \
+    wget -O /usr/bin/brook https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 && \
+    wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - && \
+    chmod +x /usr/bin/v2ray-plugin_linux_amd64 /usr/bin/gost /usr/bin/brook && \
+    chmod +x /v2ray /v2ctl && \
+    rm -rf /var/cache/apk/*
 
-CMD /entry.sh
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
 
-
-
-
-
+CMD /start.sh
---
+++ start.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# config caddy
+mkdir -p /usr/share/caddy
+wget -O /usr/share/caddy/index.html https://raw.githubusercontent.com/ringring1/caddy-test/master/index.html
+cat << EOF > /etc/caddy/Caddyfile
+:$PORT
+root * /usr/share/caddy
+file_server
+
+@websocket_gost {
+header Connection *Upgrade*
+header Upgrade    websocket
+path $GOSTPATH
+}
+reverse_proxy @websocket_gost 127.0.0.1:2234
+
+@websocket_v2ray {
+header Connection *Upgrade*
+header Upgrade    websocket
+path /ring
+}
+reverse_proxy @websocket_v2ray 127.0.0.1:9090
+EOF
+
+# config v2ray
+cat << EOF > /v2ray.json
+{
+    "inbounds": 
+    [
+        {
+            "port": 9090,"listen": "127.0.0.1","protocol": "vmess",
+            "settings": {"clients": [{"id": "$UUID", "alterId": 64}]},
+            "streamSettings": {"network": "ws", "security": "auto", "wsSettings": {"path": "/ring"}}
+        }
+    ],
+    "outbounds": [{"protocol": "freedom"}]
+}	
+EOF
+
+# start
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+
+#gost -L ss+ws://AEAD_CHACHA20_POLY1305:$PASSWORD@127.0.0.1:2234?path=$GOSTPATH & test&ok 
+#client& ./gost -L :8888 -F=ss+wss://AEAD_CHACHA20_POLY1305:password@***.herokuapp.com:443?path=/gostpath
+
+#gost -L http+ws://admin:123456@127.0.0.1:2234?path=$GOSTPATH & &test & ok
+#client& ./gost -L :8888 -F=http+wss://admin:123456@***.herokuapp.com:443?path=/gostpath
+
+#gost -L ss2://AEAD_CHACHA20_POLY1305:password@127.0.0.1:2234?path=$GOSTPATH &
+gost -L ss+ws://AEAD_CHACHA20_POLY1305:password@127.0.0.1:2234?path=$GOSTPATH &
+
+/v2ray -config /v2ray.json
