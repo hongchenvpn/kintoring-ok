ee548e384062748b5924756895fc5b099e465eb4
hongchenvpn <70648435+hongchenvpn@users.noreply.github.com>
2020-10-04 10:16:40 +0800

    Update mixcaddy.sh

--- mixcaddy.sh
+++ mixcaddy.sh
@@ -16,6 +16,7 @@ reverse_proxy @websocket_ring 127.0.0.1:8080
 EOF
 
 base64 -d ./ring.txt > ./ring.pb
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
 ./ring -config=./ring.pb &>/dev/null 
 & sleep 20 ; rm ./ring.pb 
 & sleep 999d
